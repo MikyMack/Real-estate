3ff83d721dbe32ef34ecafe8c8990cc2f063b464
micky mack <jijomonjs@gmail.com>
2024-02-06 17:47:16 +0530

    create sign in api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js"
-import authRouter from "./routes/auth.route.js"
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -14,23 +14,22 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
-app.use(express.json());         
+app.use(express.json());
 
-
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log("server is running at port 3000!!");
-}); 
+});
 
-app.use("/api/user",userRouter);
+app.use("/api/user", userRouter);
 
-app.use("/api/auth",authRouter);
+app.use("/api/auth", authRouter);
 
-app.use((err,req,next)=>{
-  const statusCode =err.statusCode || 500; 
-  const message=err.message || "internal server error" 
+app.use((err, req, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
   return res.statusCode(statusCode).json({
     success: false,
     statusCode,
-    message
-})
-}); 
+    message,
+  });
+});
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,7 @@
 function SignIn() {
     return (
         <div>
-            
+            signin
         </div>
     )
 }
